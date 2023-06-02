3041023d68955d642ec55f442eaa502c35f1bdeb
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-06-03 02:15:40 +0300

    Created and configured client

---
+++ MoneyTracker.API/client/src/api/queries/tmp.ts
@@ -0,0 +1,3 @@
+export const query = `
+    Тут повинен бути gql запит
+`
---
+++ MoneyTracker.API/client/src/components/ExampleComponent/ExampleComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExampleElement from "../../elements/ExampleElement/ExampleElement";
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            This is Example Component
+            <ExampleElement/>
+        </div>
+
+    );
+};
+
+export default ExampleComponent;
