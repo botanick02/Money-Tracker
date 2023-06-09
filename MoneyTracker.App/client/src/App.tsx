f0c2396fc2160ca0771fa1c6b7782a5e06a61144
Andrii Hretskyi <andreygretzky@gmail.com>
2023-06-09 20:27:30 +0300

    add   registration Route

--- MoneyTracker.App/client/src/App.tsx
+++ MoneyTracker.App/client/src/App.tsx
@@ -7,37 +7,43 @@ import BalanceComponent from "./pages/Balance/BalanceComponent";
 import { RefreshTokenReducer } from "./store/Example/Reducers/RefreshTokenReducer";
 import { UserReducer } from "./store/Example/Reducers/UserReducer";
 import { checkTokenExpire } from "./tools/checkTokenExpire";
+import Registration from "./pages/Registration/Registration";
 
 const { GET_ACCESS_TOKEN } = RefreshTokenReducer.actions;
 const { GET_USER_INFO } = UserReducer.actions;
 
-
 function App() {
   const isAuth = useAppSelector((state) => state.Authorization.isAuth);
-  const accessTokenRefreshing = useAppSelector((state) => state.RefreshToken.loading);
-console.log(accessTokenRefreshing)
-    const dispatch = useAppDispatch();
-    useEffect(() => {
-      if (checkTokenExpire()) {
-          if (!accessTokenRefreshing && isAuth) {
-              dispatch(GET_ACCESS_TOKEN());
-          }
+  const accessTokenRefreshing = useAppSelector(
+    (state) => state.RefreshToken.loading
+  );
+  console.log(accessTokenRefreshing);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (checkTokenExpire()) {
+      if (!accessTokenRefreshing && isAuth) {
+        dispatch(GET_ACCESS_TOKEN());
       }
+    }
   }, []);
 
   useEffect(() => {
-      if (isAuth) {
-          dispatch(GET_USER_INFO());
-      }
+    if (isAuth) {
+      dispatch(GET_USER_INFO());
+    }
   }, [isAuth]);
-console.log(isAuth)
+  console.log(isAuth);
   return (
     <BrowserRouter>
-    
-  
       <Routes>
-       
-        <Route path={"/"} element=  {isAuth ? <BalanceComponent /> : <SignInForm />} />
+        <Route
+          path={"/registration"}
+          element={isAuth ? <BalanceComponent /> : <Registration />}
+        />
+        <Route
+          path={"/"}
+          element={isAuth ? <BalanceComponent /> : <SignInForm />}
+        />
       </Routes>
     </BrowserRouter>
   );
