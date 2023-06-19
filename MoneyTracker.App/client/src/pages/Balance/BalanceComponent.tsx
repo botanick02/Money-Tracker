cf00248d322f4806b4d35606529d85e425958b27
Andrii Hretskyi <andreygretzky@gmail.com>
2023-06-19 14:16:07 +0300

    style Sign Up and login pages

--- MoneyTracker.App/client/src/pages/Balance/BalanceComponent.tsx
+++ MoneyTracker.App/client/src/pages/Balance/BalanceComponent.tsx
@@ -2,22 +2,23 @@ import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { AuthorizationReducer } from "../../store/Example/Reducers/AuthorizationReducer";
 import "../../styles/BalanceComponent.scss";
 import Transactions from "../Transactions/Transactions";
-
+import { useNavigate } from "react-router";
 const BalanceComponent = () => {
-  const accountBalance = 5000;
-  const spentCategories = [
-    { name: 'Їжа', amount: 1000 },
-    { name: 'Транспорт', amount: 500 },
-    { name: 'РОзваги', amount: 200 },
-  ];
+
   const { SIGN_OUT } = AuthorizationReducer.actions;
   const dispatch = useAppDispatch();
+  const navigate = useNavigate ();
+
 
   return (
  
     <div>
     <Transactions />
-    <button className="button danger" onClick={() => dispatch(SIGN_OUT())}>
+    <button className="button " onClick={() =>
+        {
+      dispatch(SIGN_OUT())
+      navigate('/')
+      }}>
         Sign Out
       </button>
   </div>
