803f87f20278e851ca5bc470da01306d8f4102d1
Andrii Hretskyi <andreygretzky@gmail.com>
2023-06-20 23:07:06 +0300

    fix conflicts

--- MoneyTracker.App/client/src/pages/SignIn/SignInForm.tsx
+++ MoneyTracker.App/client/src/pages/SignIn/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {IUserType, UserLoginType} from '../../types/IUserType';
 import '../../styles/Registration.scss';
 import {AuthorizationReducer} from '../../store/Example/Reducers/AuthorizationReducer';
@@ -6,8 +6,10 @@ import {useAppDispatch, useAppSelector} from '../../hooks/useAppDispatch';
 import {useForm} from "react-hook-form";
 import {useNavigate} from 'react-router-dom';
 import InputWrapper from "../../elements/InputWrapper";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 
-const {SIGN_IN, SIGN_IN_SUCCESS, SIGN_IN_ERROR} = AuthorizationReducer.actions;
+
+const {SIGN_IN, SIGN_IN_SUCCESS, SIGN_IN_ERROR,SIGN_IN_GOOGLE} = AuthorizationReducer.actions;
 const SignInForm = () => {
     const {
         register,
@@ -15,17 +17,36 @@ const SignInForm = () => {
             errors,
         },
         handleSubmit,
-
+        setError
     } = useForm()
 
     const serverError = useAppSelector((state) => state.Registration.error);
     const IsSinging = useAppSelector((state) => state.Authorization.loading);
     const dispatch = useAppDispatch();
 
-    const SignIn = (data: any) => {
-        console.log(data)
-        dispatch(SIGN_IN({email: data.email, password: data.password}));
-    };
+  const SignIn = (data: any) => {
+    console.log(data)
+    dispatch(SIGN_IN({email: data.email, password: data.password}));
+};
+useEffect(() => {
+  if (serverError) {
+    setError("serverError", { message: "Wrong login or password" });
+  }
+}, [serverError, setError]);
+
+  const signInGoogle = (response: CredentialResponse) => {
+    if (response.credential){
+      dispatch(SIGN_IN_GOOGLE({ token: response.credential} ));
+    }
+    else{
+      console.log("No credential in google response");
+    }
+  };
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/registration");
+  };
+
 
     return (
         <div className="sign-up-mobile">
