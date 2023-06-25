9fcb2dcad06665d80a9f6083c38699d452b20bef
Danyil Tuhaienko <botanick02@gmail.com>
2023-06-25 11:43:21 +0200

    fix: the "under-posting" problem

--- MoneyTracker.App/GraphQl/Auth/Types/Inputs/UserCreateInput.cs
+++ MoneyTracker.App/GraphQl/Auth/Types/Inputs/UserCreateInput.cs
@@ -5,11 +5,11 @@ namespace MoneyTracker.App.GraphQl.Auth.Types.Inputs
     public class UserCreateInput
     {
         [Required(ErrorMessage = "Name is required")]
-        public string? Name { get; set; } = string.Empty;
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
-        public string? Email { get; set; } = string.Empty;
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password must be at least 6 characters long")]
