51cbb3bb532ef942ddf27fa35fa84b6c9162957d
Andrii Hretskyi <andreygretzky@gmail.com>
2023-07-05 10:02:11 +0300

    conect categorylist and editcategory

--- MoneyTracker.App/client/src/components/CategoryList/CategoryList.tsx
+++ MoneyTracker.App/client/src/components/CategoryList/CategoryList.tsx
@@ -6,35 +6,35 @@ import CategoryCreate from "../CategoryCreate/CategoryCreate";
 
 const CategoryList = () => {
   const items = useAppSelector((state) => state.Category.categories);
-  const { FETCH_CATEGORIES } = CategoryItemReducer.actions;
-
+  const { FETCH_CATEGORIES,EDIT_CATEGORY } = CategoryItemReducer.actions;
+  const editSuccess = useAppSelector((state) => state.Category.editSuccess)
   const [defaultTransaction, setDefaultTransaction] = useState<
     "expense" | "income"
   >("expense");
   const [name, setName] = useState<string>("");
-
+  const [id, setId] = useState<string>("");
   const [isCreatePopupOpen, setIsCreatePopupOpen] = useState<boolean>(false);
   const handlePopupOpen = () => {
     document.body.classList.toggle("no-scroll");
     setIsCreatePopupOpen((prevState) => !prevState);
   };
 
-  const handleCategoryItemClick = (item:any) => {
-  
+  const handleCategoryItemClick = (item: any) => {
     setDefaultTransaction(item.type.toLowerCase());
-      setName(item.name);
-      console.log(name)
-    
+    setName(item.name);
+    setId(item.id);
   };
 
   const dispatch = useAppDispatch();
 
   const page = 1;
   const countOfElements = 5;
-
+  useEffect(() => {
+    console.log(name, id);
+  }, [name, id]);
   useEffect(() => {
     dispatch(FETCH_CATEGORIES({ page, countOfElements }));
-  }, [dispatch, page, countOfElements]);
+  }, [page, countOfElements,editSuccess]);
 
   return (
     <div className="transaction-list">
@@ -43,12 +43,13 @@ const CategoryList = () => {
           transactionDefaultType={defaultTransaction}
           openPopupHandle={handlePopupOpen}
           name={name}
+        id={id}
          
         />
       )}
       {items.map((item, index) => (
         <CategorySetsItemItem
-          key={item.id}
+          key={index}
           category={item}
           onClick={() => {
             console.log(item)
