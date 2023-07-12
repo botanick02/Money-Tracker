fb854fd53be89f743896cad353de9e6ecd2c6bd2
Andrii Hretskyi <andreygretzky@gmail.com>
2023-07-12 04:19:06 +0300

    little fix

--- MoneyTracker.App/client/src/store/Example/Reducers/AccountReducer.ts
+++ MoneyTracker.App/client/src/store/Example/Reducers/AccountReducer.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useAppSelector } from "../../../hooks/useAppDispatch";
 
 export interface Account {
-  actualAccount: string | null;
+  actualAccount: string 
   actualBalance:number
   actuaIncomelBalance:number
   actualExpenseBalance:number
