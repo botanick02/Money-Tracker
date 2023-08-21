7f292875f3010c7cfbe94c71b74ff40c39b6bc46
Danyil Tuhaienko <botanick02@gmail.com>
2023-08-22 00:04:07 +0300

    refactor: put graph types and inputs in one file

--- MoneyTracker.App/GraphQl/FinancialOperations/Types/Inputs/DebitOperationInputType.cs
+++ MoneyTracker.App/GraphQl/FinancialOperations/Types/Inputs/DebitOperationInputType.cs
@@ -1,7 +1,27 @@
 ﻿using GraphQL.Types;
+using MoneyTracker.App.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace MoneyTracker.App.GraphQl.FinancialOperation.Types.Inputs
 {
+    public class DebitOperationInput
+    {
+        public string? Title { get; set; }
+
+        public string? Note { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [GuidValidationAttribute(ErrorMessage = "CategoryId is invalid")]
+        public string CategoryId { get; set; }
+
+        [GuidValidationAttribute(ErrorMessage = "ToAccountId is invalid")]
+        public string ToAccountId { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+
     public class DebitOperationInputType : InputObjectGraphType<DebitOperationInput>
     {
         public DebitOperationInputType()
--- MoneyTracker.App/GraphQl/FinancialOperations/Types/Inputs/TransferOperationInputType.cs
+++ MoneyTracker.App/GraphQl/FinancialOperations/Types/Inputs/TransferOperationInputType.cs
@@ -1,7 +1,30 @@
 ﻿using GraphQL.Types;
+using MoneyTracker.App.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace MoneyTracker.App.GraphQl.FinancialOperation.Types.Inputs
 {
+    public class TransferOperationInput
+    {
+        public string? Title { get; set; }
+
+        public string? Note { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [GuidValidationAttribute(ErrorMessage = "CategoryId is invalid")]
+        public string CategoryId { get; set; }
+
+        [GuidValidationAttribute(ErrorMessage = "FromAccountId is invalid")]
+        public string FromAccountId { get; set; }
+
+        [GuidValidationAttribute(ErrorMessage = "FromAccountId is invalid")]
+        public string ToAccountId { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+
     public class TransferOperationInputType : InputObjectGraphType<TransferOperationInput>
     {
         public TransferOperationInputType()
