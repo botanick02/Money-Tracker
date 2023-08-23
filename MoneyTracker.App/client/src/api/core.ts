5ba010b1b5d58e48f22b0f765a468ffded36fcac
Andrii Hretskyi <andreygretzky@gmail.com>
2023-08-23 08:17:54 +0300

    add interval to REFRESH_ACCESS_TOKEN

--- MoneyTracker.App/client/src/api/core.ts
+++ MoneyTracker.App/client/src/api/core.ts
@@ -1,12 +1,8 @@
-import { useAppDispatch } from "../hooks/useAppDispatch";
-import { TOKEN_EXPIRE } from "../store/Auth/Auth.slice";
-import { checkTokenExpire } from "../tools/checkTokenExpire"
-const dispatch = useAppDispatch();
 
 const baseURL = "https://localhost:7299/graphql"
 
 export const request = async (query?: string, variables?: any) => {
-    dispatch(TOKEN_EXPIRE(checkTokenExpire()))
+ 
 
 
     if(!query)
