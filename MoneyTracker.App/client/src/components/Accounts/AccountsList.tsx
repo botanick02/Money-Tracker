21cd999c3381feb4a586b7bd64c1ee2291276f1c
Andrii Hretskyi <andreygretzky@gmail.com>
2023-08-23 11:50:14 +0300

    accounts setting style

--- MoneyTracker.App/client/src/components/Accounts/AccountsList.tsx
+++ MoneyTracker.App/client/src/components/Accounts/AccountsList.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/useAppDispatch";
-import AccountCreate from "./AccountCreate"; // Make sure to import the correct path
-import { FETCH_ACCOUNTS } from "../../store/Account/Account.slice"; // Adjust the import accordingly
+import AccountCreate from "./AccountCreate";
+import { FETCH_ACCOUNTS } from "../../store/Account/Account.slice";
+import StatsTransactionItem from "../../elements/StatsTransactionItem";
 
 const AccountsList = () => {
   const accounts = useAppSelector((state) => state.Account.accounts);
@@ -16,25 +17,30 @@ const AccountsList = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(FETCH_ACCOUNTS()); // Use the appropriate action for fetching accounts
+    dispatch(FETCH_ACCOUNTS());
   }, [dispatch]);
+
+ 
+  const accountsToRender = accounts.slice(0, accounts.length - 1);
 
   return (
     <main>
-      <div className="account-list">
+      <div className="transaction-list">
         {isCreatePopupOpen && (
-          <AccountCreate
-            openPopupHandle={handlePopupOpen} name={""}          />
+          <AccountCreate openPopupHandle={handlePopupOpen} name="" />
         )}
-        {accounts.map((item, index) => (
-          <div key={item.id} className="account-item">
-         
-            <span>{item.name}</span>
-          </div>
+        {accountsToRender.map((item) => (
+          <StatsTransactionItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            currency={item.currency.symbol}
+            balance={item.balance}
+          />
         ))}
 
         {!isCreatePopupOpen && (
-          <div onClick={handlePopupOpen} className="new-account button">
+          <div onClick={handlePopupOpen} className="new-transaction button">
             +
           </div>
         )}
