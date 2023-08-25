2b04325d4da06d5551c2ad178c5871e6d371175d
Danyil Tuhaienko <botanick02@gmail.com>
2023-08-25 20:59:25 +0300

    feat: time travel control popup

--- MoneyTracker.App/client/src/components/common/Layout.tsx
+++ MoneyTracker.App/client/src/components/common/Layout.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import NavigationMenu from "../common/NavigationMenu";
 import TimeTravelAlert from "./TImeTravelAlert";
+import TimeTravelPopup from "../TimeTravelPopup";
 
 const Layout = () => {
   return (
     <>
+      <TimeTravelPopup/>
       <TimeTravelAlert/>
       <Header />
       <Outlet />
--- MoneyTracker.App/client/src/components/common/TImeTravelAlert.tsx
+++ MoneyTracker.App/client/src/components/common/TImeTravelAlert.tsx
@@ -1,11 +1,19 @@
-import { useAppSelector } from "../../hooks/useAppDispatch";
+import { useAppDispatch, useAppSelector } from "../../hooks/useAppDispatch";
+import { SHOW_TIME_TRAVEL_POPUP } from "../../store/TimeTravel/TimeTravel.slice";
 
 const TimeTravelAlert = () => {
   const timeTravelValue = useAppSelector((store) => store.TimeTravel.datetime);
 
-  return timeTravelValue ? <div className={"time-travel-alert"}>
-    You are time traveling to {new Date(timeTravelValue).toLocaleString()}
-  </div> : null;
+  const dispatch = useAppDispatch();
+
+  return timeTravelValue ? (
+    <div
+      className={"time-travel-alert"}
+      onClick={() => dispatch(SHOW_TIME_TRAVEL_POPUP(true))}
+    >
+      You are time traveling to {new Date(timeTravelValue).toLocaleString()}
+    </div>
+  ) : null;
 };
 
 export default TimeTravelAlert;
