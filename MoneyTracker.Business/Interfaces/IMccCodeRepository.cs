daf611e41aa7ce9e590fbb72a45c5d225e9cbfa6
Danyil Tuhaienko <botanick02@gmail.com>
2023-09-12 10:23:54 +0300

    feat MCC codes json and repo

---
+++ MoneyTracker.Business/Interfaces/IMccCodeRepository.cs
@@ -0,0 +1,9 @@
+﻿using MoneyTracker.Business.Entities;
+
+namespace MoneyTracker.Business.Interfaces
+{
+    public interface IMccCodeRepository
+    {
+        public Currency GetMccNameById(string code);
+    }
+}
---
+++ MoneyTracker.DataAccess/Repositories/MccCodeRepository.cs
@@ -0,0 +1,32 @@
+﻿using MoneyTracker.Business.Entities;
+using MoneyTracker.Business.Interfaces;
+using Newtonsoft.Json;
+
+namespace MoneyTracker.DataAccess.Repositories
+{
+    public class MccCodeRepository : IMccCodeRepository
+    {
+        private readonly IEnumerable<Currency> mccCodes;
+
+        public MccCodeRepository()
+        {
+            var path = @"../MoneyTracker.DataAccess/Resources/MccCodes.json";
+            var readMccCodes = JsonConvert.DeserializeObject<List<Currency>>(File.ReadAllText(path));
+            if (readMccCodes == null)
+            {
+                throw new FileNotFoundException("MccCodes were failed to receive");
+            }
+            mccCodes = readMccCodes;
+        }
+
+        public Currency GetMccNameById(string code)
+        {
+            if (!mccCodes.Any(c => c.Code == code))
+            {
+                throw new ArgumentOutOfRangeException(nameof(code), "Invalid MCC code");
+            }
+
+            return mccCodes.FirstOrDefault(c => c.Code == code)!;
+        }
+    }
+}
